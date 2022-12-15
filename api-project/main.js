de1aec1aad848b6e1369cfd7f656ee0718fad823
ericl2055 <eric_lin2055@icloud.com>
2022-12-15 13:59:28 -0500

    api

--- api-project/main.js
+++ api-project/main.js
@@ -1,6 +1,16 @@
-const URL = "http://taco-randomizer.herokuapp.com/";
+const URL = "https://www.boredapi.com/api/activity";
+const DOM = {
+  api: document.getElementById("api"),
+};
 
 async function getData(URL) {
-  const response = await fetch(URL);
-  console.log(response);
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    DOM.api.insertAdjacentHTML;
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
 }
+getData(URL);
