23b72bca0681c0c38fb5917c63fa8cfeb62f6e97
ericl2055 <eric_lin2055@icloud.com>
2022-12-16 13:52:52 -0500

    api

---
+++ api-project/DOM.js
@@ -0,0 +1,8 @@
+const DOM = {
+  custom: document.getElementById("custom"),
+  activity: document.getElementById("activity"),
+  type: document.getElementById("type"),
+  participants: document.getElementById("participants"),
+  price: document.getElementById("price"),
+};
+export { DOM };
--- api-project/main.js
+++ api-project/main.js
@@ -1,16 +1,44 @@
 const URL = "https://www.boredapi.com/api/activity";
-const DOM = {
-  api: document.getElementById("api"),
-};
 
+function api() {}
 async function getData(URL) {
   try {
     const response = await fetch(URL);
     const data = await response.json();
-    DOM.api.insertAdjacentHTML;
+    let activity = data.activity;
+    let type = data.type;
+    let participants = data.participants;
+    let price = data.price;
+    DOM.activity.insertAdjacentHTML(
+      "afterend",
+      `<div class = "card" id = "card">
+      <h1>${activity}</h1>   </div>`
+    );
+    DOM.type.insertAdjacentHTML(
+      "afterend",
+      `<div class = "card" id = "card">
+     <h2>${type}</h2>
+      </div>`
+    );
+    DOM.participants.insertAdjacentHTML(
+      "afterend",
+      `<div class = "card" id = "card">
+    <h2>${participants}</h2>
+      </div>`
+    );
+    DOM.price.insertAdjacentHTML(
+      "afterend",
+      `<div class = "card" id = "card">
+     <h2>${price}</h2>
+      </div>`
+    );
     console.log(data);
   } catch (error) {
-    console.log(error);
+    DOM.custom.insertAdjacentHTML(
+      "beforebegin",
+      `<h5>Sorry, no activity provided.</h5>`
+    );
   }
 }
 getData(URL);
+import { DOM } from "./DOM";
