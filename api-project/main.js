bd792e59ef529532d2597db9727f1277f47a1762
ericl2055 <eric_lin2055@icloud.com>
2022-12-21 13:59:48 -0500

    api

--- api-project/main.js
+++ api-project/main.js
@@ -1,39 +1,30 @@
-const randomURL = "https://www.boredapi.com/api/activity";
-const typeURL = "http://www.boredapi.com/api/activity?type=";
-const partURL1 = "http://www.boredapi.com/api/activity?participants=1";
-const partURL2 = "http://www.boredapi.com/api/activity?participants=2";
-const partURL3 = "http://www.boredapi.com/api/activity?participants=3";
-const partURL4 = "http://www.boredapi.com/api/activity?participants=4";
-const partURL5 = "http://www.boredapi.com/api/activity?participants=5";
-
 DOM.partoutput.innerHTML = DOM.slider.value;
 DOM.slider.oninput = function () {
   DOM.partoutput.innerHTML = this.value;
 };
+
 DOM.submit.addEventListener("click", function () {
-  if (DOM.inputtype.value == "1") {
+  const URL = `http://www.boredapi.com/api/activity?participants=${DOM.slider.value}`;
+  if (DOM.selecttype.value == "1") {
     remove();
-    filterparts();
+    getparticipant(URL);
   }
 });
 DOM.new.addEventListener("click", function () {
   remove();
   getrandom(randomURL);
 });
-function filterparts() {
-  if (DOM.slider.value == "1") {
-    get1participant(typeURL);
-  }
-}
+
 function remove() {
   let card = document.querySelectorAll("#card");
   card.forEach((card) => {
     card.remove();
   });
 }
-async function get1participant(partURL1) {
+
+async function getparticipant(URL) {
   try {
-    const response = await fetch(partURL1);
+    const response = await fetch(URL);
     const data = await response.json();
     let activity = data.activity;
     let type = data.type;
@@ -64,6 +55,7 @@ async function get1participant(partURL1) {
     );
   }
 }
+
 async function getrandom(randomURL) {
   try {
     const response = await fetch(randomURL);
@@ -97,6 +89,7 @@ async function getrandom(randomURL) {
     );
   }
 }
-//get1participant(partURL1);
+
 getrandom(randomURL);
 import { DOM } from "./DOM";
+import { randomURL } from "./URLS.JS";
