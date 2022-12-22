f847dac26b70ab35e963aea6f227922317da0b2f
ericl2055 <eric_lin2055@icloud.com>
2022-12-22 13:04:44 -0500

    api

--- api-project/main.js
+++ api-project/main.js
@@ -1,15 +1,21 @@
-DOM.partoutput.innerHTML = DOM.slider.value;
-DOM.slider.oninput = function () {
-  DOM.partoutput.innerHTML = this.value;
-};
+const randomURL = "https://www.boredapi.com/api/activity";
+let submit = document.querySelectorAll("submit");
 
-DOM.submit.addEventListener("click", function () {
+function submit() {
+  card = document.querySelectorAll("#customize");
+  card.forEach((button) => {
+    button.addEventListener("click", function () {
   const URL = `http://www.boredapi.com/api/activity?participants=${DOM.slider.value}`;
+  const URL2 = `http://www.boredapi.com/api/activity?type=${DOM.selecttype.value}`;
   if (DOM.selecttype.value == "1") {
     remove();
     getparticipant(URL);
-  }
-});
+  } else {
+    remove();
+    gettype(URL2);
+  }}
+})
+
 DOM.new.addEventListener("click", function () {
   remove();
   getrandom(randomURL);
@@ -20,6 +26,45 @@ function remove() {
   card.forEach((card) => {
     card.remove();
   });
+}
+function removecustom() {
+  let card = document.querySelectorAll("#customize");
+  card.forEach((card) => {
+    card.remove();
+  });
+}
+async function gettype(URL2) {
+  try {
+    const response = await fetch(URL2);
+    const data = await response.json();
+    let activity = data.activity;
+    let type = data.type;
+    let participants = data.participants;
+    DOM.activity.insertAdjacentHTML(
+      "afterend",
+      `<div class = "card" id = "card">
+      <h3>${activity}</h3>   </div>`
+    );
+    DOM.type.insertAdjacentHTML(
+      "afterend",
+      `<div class = "card" id = "card">
+     <h3>${type}</h3>
+      </div>`
+    );
+    DOM.participants.insertAdjacentHTML(
+      "afterend",
+      `<div class = "card" id = "card">
+    <h3>${participants}</h2>
+      </div>`
+    );
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    DOM.custom.insertAdjacentHTML(
+      "beforebegin",
+      `<h5>Sorry, no activity provided.</h5>`
+    );
+  }
 }
 
 async function getparticipant(URL) {
@@ -89,7 +134,48 @@ async function getrandom(randomURL) {
     );
   }
 }
+DOM.buttontype.addEventListener("click", function () {
+  removecustom();
+  DOM.custom.insertAdjacentHTML(
+    "beforeend",
+    `<div id="customize"><h3>Select Type:</h3>
+  <select type="input" id="inputtype">
+    <option value="1">Select</option>
+    <option value="busywork">busywork</option>
+    <option value="charity">charity</option>
+    <option value="cooking">cooking</option>
+    <option value="diy">diy</option>
+    <option value="education">education</option>
+    <option value="music">music</option>
+    <option value="recreational">recreational</option>
+    <option value="relaxation">relaxation</option>
+    <option value="social">social</option>
+  </select></div>`
+  );
+});
 
+DOM.buttonpart.addEventListener("click", function () {
+  removecustom();
+  DOM.custom.insertAdjacentHTML(
+    "beforeend",
+    `<div id="customize"><h3>Amount of Participants:</h3>
+    <div class="slidecontainer">
+      <input
+        type="range"
+        min="1"
+        max="5"
+        value="3"
+        class="slider"
+        id="participants"
+      />
+      <h3 id="demo"></h3>
+      <button id="submit">Submit</button>
+    </div></div>`
+  );
+  DOM.partoutput.innerHTML = DOM.slider.value;
+  DOM.slider.oninput = function () {
+    DOM.partoutput.innerHTML = this.value;
+  };
+});
 getrandom(randomURL);
 import { DOM } from "./DOM";
-import { randomURL } from "./URLS.JS";
